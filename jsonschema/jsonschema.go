4a8b80380cf3e5115b05680b005e2ad232197f58
Merlin Cox <merlin.cox@denma.digital>
2018-12-07 17:30:38 +0000

    Make nsk argument optional

--- jsonschema/jsonschema.go
+++ jsonschema/jsonschema.go
@@ -52,8 +52,8 @@ func (s *Schema) Type() (firstOrDefault string, multiple bool) {
 	return "", multiple
 }
 
-// Parse parses a JSON schema from a string.
-func Parse(schema string, nsk bool) (*Schema, error) {
+// Parse parses a JSON schema from a string, with an optional boolean argument to permit schemas without a $schema key.
+func Parse(schema string, nsk ...bool) (*Schema, error) {
 	s := &Schema{}
 	err := json.Unmarshal([]byte(schema), s)
 
@@ -61,8 +61,8 @@ func Parse(schema string, nsk bool) (*Schema, error) {
 		return s, err
 	}
 
-	if !nsk && s.SchemaType == "" {
-		return s, errors.New("JSON schema must have a $schema key unless -nsk flag is set")
+	if (len(nsk) == 0 || nsk[0] == false) && s.SchemaType == "" {
+		return s, errors.New("JSON schema must have a $schema key unless nsk flag is set")
 	}
 
 	return s, err
